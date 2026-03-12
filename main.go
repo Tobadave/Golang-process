b1555239b1f45197df1fce369bb5d3e80699f1ca
dolorunt2 <dolorunt@acad.learn2earn.ng>
2026-03-12 05:00:43 +0100

    moving to git buffio

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	// "errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	if len(os.Args) != 2 {
+		fmt.Println("Error: not enough args")
+		os.Exit(1) //error 1 means incomplete return code, 0 means succesfull
+	}
+
+	content, err := os.ReadFile("Sample.txt")
+	if err != nil {
+		fmt.Println("ERROR 1 :", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("----------------------------")
+
+	err = os.WriteFile("result.txt", []byte("You are going home"), 0644)
+	if err != nil {
+		fmt.Println("error writing file:", err)
+		os.Exit(1)
+	}
+
+	newContent := string(content)
+	fmt.Println(newContent)
+	fmt.Println("-----Code succesful-------")
 }

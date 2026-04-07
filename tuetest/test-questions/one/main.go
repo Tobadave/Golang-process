2f484210e2db500be915bd23add12b5afe11f2b1
dolorunt2 <dolorunt@acad.learn2earn.ng>
2026-04-07 22:35:04 +0100

    sample-qustions

---
+++ tuetest/test-questions/one/main.go
@@ -0,0 +1,12 @@
+package main
+
+// Write a Go program that reads a file called words.txt, splits it by newlines, and prints each line with its line number in front.
+// Example output:
+// 1: Hello
+// 2: World
+// 3: Goodbye
+
+func main() {
+
+}
+

c684aa3fd0d97ebed2230921053b6284d0a5a189
dolorunt2 <tobadave11@gmail.com>
2026-04-27 16:40:50 +0100

    added

---
+++ functions/nooftimes.go
@@ -0,0 +1,30 @@
+package main
+import "fmt"
+
+func main() {
+  fmt.Println("Try programiz.pro")
+  
+  sample := "hello..world"
+//   fmt.Println(mytrim(sample))
+  mytrim(sample)
+}
+
+func mytrim(s string) {
+    look := map[rune]bool{
+        '.': true,
+        ',': true,
+        ';': true,
+    }
+
+    result := map[rune]int{} // stores char and count
+
+    for _, ch := range s {
+        if _, ok := look[ch]; ok {
+            result[ch]++ // increments count each time char is found
+        }
+    }
+    
+    for char, count := range result {
+        fmt.Printf("%q: %d\n", char, count) //returns ----> ('.' : 2)
+    }
+}
